162d3623577f9462f86d5699ff8bab6b3962b3d5
gerrrderrr <olgagerrrderrr@gmail.com>
2023-03-28 12:25:52 +0300

    Changed one endpoint in antMatchers to multiple and added csrf token to the form

--- src/main/java/ru/netology/hibernate/configuration/SecurityConfiguration.java
+++ src/main/java/ru/netology/hibernate/configuration/SecurityConfiguration.java
@@ -20,24 +20,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
                 .and()
-                .authorizeRequests().antMatchers("persons/create-new-person").permitAll()
+                .authorizeRequests().antMatchers("persons/create-new-person",
+                        "persons/create").permitAll()
                 .and()
-                .authorizeRequests().antMatchers("persons/create").permitAll()
+                .authorizeRequests().antMatchers("persons/all-users",
+                        "persons/by-city",
+                        "persons/by-age",
+                        "persons/by-name-surname").hasAuthority("read")
                 .and()
-                .authorizeRequests().antMatchers("persons/all-users").hasAuthority("read")
+                .authorizeRequests().antMatchers("persons/update-phone",
+                        "persons/update-city",
+                        "persons/delete").hasAuthority("write")
                 .and()
-                .authorizeRequests().antMatchers("persons/update-phone").hasAuthority("write")
-                .and()
-                .authorizeRequests().antMatchers("persons/update-city").hasAuthority("write")
-                .and()
-                .authorizeRequests().antMatchers("persons/delete").hasAuthority("write")
-                .and()
-                .authorizeRequests().antMatchers("persons/by-city").hasAuthority("read")
-                .and()
-                .authorizeRequests().antMatchers("persons/by-age").hasAuthority("read")
-                .and()
-                .authorizeRequests().antMatchers("persons/by-name-surname").hasAuthority("read")
-                .and()
-                .authorizeRequests().anyRequest().authenticated().and().csrf().disable();
+                .authorizeRequests().anyRequest().authenticated();
     }
 }
